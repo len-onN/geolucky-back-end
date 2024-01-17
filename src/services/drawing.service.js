dcc1768bdf35f58282ddca7868fcff94698b81b4
len-onN <lenonphilippi@gmail.com>
2024-01-16 21:39:43 -0300

    timezone Brasília

--- src/services/drawing.service.js
+++ src/services/drawing.service.js
@@ -4,23 +4,42 @@ const schedule = require('node-schedule');
 const geolib = require('geolib');
 const { getRaffleDetails } = require('./raffles.service');
 const transporter = require('../utils/email');
-// const time = require('time');
+const moment = require('moment-timezone');
 
 const generateWeekDates = () => {
-  const dataAtual = new Date();
-  const dataFinal = new Date(dataAtual);
-  dataFinal.setUTCHours(19, 59, 59, 999);
-  const dataInicial = new Date(dataFinal);
-  dataInicial.setUTCHours(20, 0, 0, 0);
-  dataInicial.setDate(dataFinal.getDate() - 6);
+  const nowDate = moment();
+  const timezone = 'America/Sao_Paulo'; // Fuso horário de São Paulo (Brasília)
 
-  return { dataInicial, dataFinal };
+  const finalDate = nowDate.clone().tz(timezone).endOf('day').set({
+    hour: 19,
+    minute: 59,
+    second: 59,
+    millisecond: 999
+  });
+  const initialDate = finalDate.clone().subtract(6, 'days').startOf('day').set({
+    hour: 20,
+    minute: 0,
+    second: 0,
+    millisecond: 0
+  });
+
+  return { initialDate, finalDate };
 };
+
+// const generateWeekDates = () => {
+//   const dataAtual = new Date();
+//   const dataFinal = new Date(dataAtual);
+//   dataFinal.setUTCHours(19, 59, 59, 999);
+//   const dataInicial = new Date(dataFinal);
+//   dataInicial.setUTCHours(20, 0, 0, 0);
+//   dataInicial.setDate(dataFinal.getDate() - 6);
+
+//   return { dataInicial, dataFinal };
+// };
 
 const getRandomCoordinates = () => {
   const randomLat = parseFloat((Math.random() * (90 + 90) - 90).toFixed(8));
   const randomLng = parseFloat((Math.random() * (180 + 180) - 180).toFixed(8));
-  console.log("randomLat", randomLat, "randomLng", randomLng);
   return { latitude: randomLat, longitude: randomLng };
 };
 
@@ -30,23 +49,16 @@ const findClosestPoint = (randomCoords, points) => {
 };
 
 const saveRaffleData = async (winnerPoint, randomCoords, competingPoints) => {
-  console.log("data: ", winnerPoint);
-  console.log("data: ", competingPoints);
-  const { dataInicial, dataFinal } = generateWeekDates();
-  // const dataInicial = new Date();
-  // const dataFinal = new Date();
-  console.log(dataInicial, dataFinal);
+  const { initialDate, finalDate } = generateWeekDates();
   try {
-    // Crie um novo registro na tabela Raffle
     const raffle = await Raffle.create({
       winnerPointId: winnerPoint.userId,
       drawnLat: randomCoords.latitude,
       drawnLng: randomCoords.longitude,
-      drawnStart: dataInicial,
-      drawnEnd: dataFinal,
+      drawnStart: initialDate,
+      drawnEnd: finalDate,
     });
 
-    // Associe os pontos concorrentes a esse sorteio na tabela RafflePoints
     await RafflePoints.bulkCreate(competingPoints.map((point) => ({
       pointId: point.id,
       raffleId: raffle.id,
@@ -60,13 +72,13 @@ const saveRaffleData = async (winnerPoint, randomCoords, competingPoints) => {
 };
 
 const findWeekRafflePoints = async () => {
-  const { dataInicial, dataFinal } = generateWeekDates();
+  const { initialDate, finalDate } = generateWeekDates();
 
   try {
     const results = await Point.findAll({
       where: {
         createdAt: {
-          [Op.between]: [dataInicial, dataFinal],
+          [Op.between]: [initialDate, finalDate],
         },
       },
       include: [{
@@ -84,18 +96,15 @@ const findWeekRafflePoints = async () => {
   }
 };
 
-schedule.scheduleJob('53 48 11 * * 1', async () => {
+schedule.scheduleJob('45 45 15 * * 2', async () => {
   console.log('Tarefa agendada');
 
   const randomCoords = getRandomCoordinates();
   console.log('Coordenadas Aleatórias:', randomCoords);
   try {
     const points = await findWeekRafflePoints();
-    // console.log("points:", points);
     const closestPoint = findClosestPoint(randomCoords, points);
     console.log('Ponto Mais Próximo:', closestPoint);
-    // const user = await User.findByPk(closestPoint.userId);
-    // console.log(user);
     await saveRaffleData(closestPoint, randomCoords, points);
     if (points.length) {
       points.forEach((point) => console.log("email: ", point.user.email));
